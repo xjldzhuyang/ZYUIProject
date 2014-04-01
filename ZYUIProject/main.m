1fd44a773ef3f2cd61882e62e618621724273180
Mofocal <mofocal@Vivian-Lius-MacBook-Pro.local>
2014-04-01 13:21:03 +0800

    Initial Commit

---
+++ ZYUIProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZYUIProject
+//
+//  Created by Mofocal on 14-4-1.
+//  Copyright (c) 2014年 jiankun.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "zyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([zyAppDelegate class]));
+    }
+}
